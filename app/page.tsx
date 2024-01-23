deab5e9618af59c016f33f375f0fcc4203524034
khrahat92 <khrahat92@gmail.com>
2024-01-24 03:27:28 +0600

    remove cache

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
   return (
     <main className="">
       <header className="sticky top-0 flex justify-center bg-green-500 text-2xl font-bold p-4 h-[10vh] z-10">
-        <h1>Hello world</h1>
+        <h1>Hello world!!!</h1>
       </header>
       <section className="h-[90vh] bg-red-500 relative md:flex md:items-center md:justify-center">
         <div className="flex flex-col justify-center items-center absolute top-[30%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] md:relative md:top-0 md:left-0 md:translate-x-0 md:translate-y-0">
